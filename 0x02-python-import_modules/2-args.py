444f5cbeeb4feaa9bccdc8f57fcdb2ea198d9726
ziliwalid <walidzili19@gmail.com>
2023-10-05 04:18:18 -0700

    second commit

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,18 +1,13 @@
 #!/usr/bin/python3
-from sys import argv
-
-def main():
-    argc = len(argv)
-    if argc < 2:
-        print("{} arguments.".format(argc - 1))
-    else:
-        if argc == 2:
-            print("{} argument:".format(argc - 1))
-        else:
-            print("{} arguments:".format(argc - 1))
-        
-        for n in range(1, argc):
-            print("{}: {}".format(n, argv[n]))
-
 if __name__ == "__main__":
-    main()
+    from sys import argv
+argc = len(argv)
+if argc < 2:
+    print("{} arguments.".format(argc - 1))
+else:
+    if argc == 2:
+        print("{} argument:".format(argc - 1))
+    else:
+        print("{} arguments:".format(argc - 1))
+    for n in range(1, argc):
+        print("{}: {}".format(n, argv[n]))
