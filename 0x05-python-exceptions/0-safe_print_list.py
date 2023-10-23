1109010e7303f446934f40c7a400d779e7c55615
ziliwalid <walidzili19@gmail.com>
2023-10-23 05:00:15 -0700

    first commit

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,7 +4,7 @@ def safe_print_list(my_list=[], x=0):
     for i in range(x):
         try:
             print(my_list[i], end='')
-            cmpt += 1
+            counter += 1
         except Exception as error:
             break
     print('')
