4b73df7bee856498e51371b0138688cee786597a
ziliwalid <walidzili19@gmail.com>
2023-10-24 03:41:30 -0700

    first commit

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3"
+
+class Square:
+    """describess a square
+    Attributes:
+        __size (int): size of a side of the square
+    """
+    def __init__(self, size):
+        """Init square
+        Args:
+            size (int): size of a side of the square
+        Returns: None
+        """
+        self.__size = size
