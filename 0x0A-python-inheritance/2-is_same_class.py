a47476f45c17f030de22dcf0d42d231404a28016
ziliwalid <walidzili19@gmail.com>
2023-11-06 10:17:08 -0800

    fixes

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -5,5 +5,5 @@ function is_same_class
 
 
 def is_same_class(obj, a_class):
-    """true if objct is a_class""
+    """true if objct is a_class"""
     return (type(obj) == a_class)
--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -3,7 +3,6 @@
 inherits_from function
 """
 
-
 def inherits_from(obj, a_class):
     """true if object is a subclass of a_class"""
     return(issubclass(type(obj), a_class) and type(obj) != a_class)
